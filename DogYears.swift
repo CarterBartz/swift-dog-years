c81b4e9fae9767a8120e2b4b94aa26dcf2499565
Carter Bartz <105955725+CarterBartz@users.noreply.github.com>
2025-05-24 23:06:09 -0500

    Create DogYears.swift

---
+++ DogYears.swift
@@ -0,0 +1,5 @@
+var dogAge = 3 // puppies age
+var earlyYears = 21 
+var laterYears = (dogAge - 2) * 4
+var humanYears = earlyYears + laterYears
+print("My Name is Bea! Ruff ruff, I am \(humanYears) years old in human years!")
